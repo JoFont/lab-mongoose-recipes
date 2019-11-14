ba3befb122c1af6d3368b4887546c504b98fddbc
Diogo Marques <diogo@fontainhas.com>
2019-11-14 17:48:06 +0000

    done

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const mongoose = require('mongoose');
-
-// Import Recipe model
 const Recipe = require('./models/Recipe');
-
-// Import data
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost/recipeApp';
 
+
+
+
+
 // Connection to the database "recipeApp"
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true })
-  .then(() => {
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(async () => {
     console.log('Connected to Mongo!');
+    
+    try {
+      await Recipe.insertMany(data);
+      await Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100});
+      await Recipe.deleteOne({title: "Carrot Cake"});
+    }
+    catch (error) {
+        console.log("error: " + error);
+    }
+    finally {
+      console.log("Succesfully updated a recipe");
+      console.log("Deleted Succesfully");
+      mongoose.connection.close()
+    }
   })
   .catch(err => {
     console.error('Error connecting to mongo', err);
